41c24c4e6a81eb72e2a8aeea3c776203add0b195
Shane Conn <shaneconn23@gmail.com>
2022-06-08 20:27:40 -0400

    python syntax

---
+++ words.py
@@ -0,0 +1,42 @@
+def print_upper_words(words):
+    """For a list of words, print out each word on a separate line,
+     but in all uppercase
+
+        >>> print_upper_words(["gotta", "catch", "them", "all"])
+        GOTTA
+        CATCH
+        THEM
+        ALL
+    """
+
+    for word in words:
+        print(word.upper())
+
+
+def print_upper_words2(words):
+    """Print each word on sep line, uppercased, if starts with E or e.
+
+        >>> print_upper_words2(["eagle", "Edward", "Alfred"])
+        EAGLE
+        EDWARD
+    """
+
+    for word in words:
+        if word.startswith("e") or word.startswith("E"):
+            print(word.upper())
+
+
+def print_upper_words3(words, must_start_with):
+    """Print each word on sep line, uppercased, if starts with one of given
+
+        >>> print_upper_words3(["eagle", "Edward", "Alfred", "zope"],
+        ...                   must_start_with=["A", "E"])
+        EDWARD
+        ALFRED
+    """
+
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
